31f1cf9359803348a5f0890d55e945514233ed50
hguandl <hguandl@gmail.com>
2018-09-21 01:45:37 +0800

    Init commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import asyncio
+import os
+import urllib.parse
+
+import parse_header
+from responses import AutoindexResponse, FileResponse, NonExistResponse
+
+
+def range_parser(part_range) -> (int, int):
+    if part_range is not None:
+        parts = part_range.split('=')
+        if parts[0] == 'bytes':
+            range_int = parts[1].split('-')
+            if range_int[0] != '':
+                start = int(range_int[0])
+            else:
+                start = 0
+            if range_int[1] != '\r\n':
+                end = int(range_int[1])
+            else:
+                end = -1
+            return start, end
+        return None
+
+
+async def dispatch(reader, writer):
+    headers_data = []
+    while True:
+        data = await reader.readline()
+        headers_data.append(data.decode())
+        print(data)
+        if data == b'\r\n' or data == b'':
+            break
+
+    client_headers = parse_header.HTTPHeader()
+    for line in headers_data:
+        client_headers.parse_header(line)
+    path = urllib.parse.unquote(client_headers.get('path'))
+    part_range = range_parser(client_headers.get('range'))
+    session_id = client_headers.get('session_id')
+
+    real_path = "." + path
+
+    try:
+        if not os.path.isfile(real_path):
+            response = AutoindexResponse(path)
+            response.add_entry('..')
+            for filename in os.listdir(real_path):
+                if filename[0:1] != '.':
+                    response.add_entry(filename)
+            writer.write(response.get_response())
+
+        else:
+            response = FileResponse(real_path, part_range, session_id)
+            writer.write(response.get_headers())
+            writer.write(response.get_content())
+
+    except FileNotFoundError:
+        response = NonExistResponse()
+        writer.writelines(response.get_response())
+
+    await writer.drain()
+    writer.close()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    coro = asyncio.start_server(dispatch, '0.0.0.0', 8000, loop=loop)
+    server = loop.run_until_complete(coro)
+
+    # Serve requests until Ctrl+C is pressed
+    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    # Close the server
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
---
+++ responses.py
@@ -0,0 +1,100 @@
+import html
+import os
+import urllib.parse
+
+from mime_types import mime_types
+
+
+class AutoindexResponse(object):
+    def __init__(self, path):
+        self.headers = ('HTTP/1.0 200 OK\r\n'
+                        'Content-Type:text/html; charset=utf-8\r\n'
+                        'Server: GH-Autoindex\r\n'
+                        'Connection: close\r\n'
+                        '\r\n')
+        self.dir = path
+        title = html.escape(path)
+        self.content = ('<html><head><title>Index of ' + title + '</title></head>\r\n'
+                       '<body bgcolor="white">\r\n'
+                       '<h1>Index of ' + title + '</h1><hr>\r\n'
+                       '<pre>\r\n')
+
+    def add_entry(self, name):
+        if not os.path.isfile('./' + self.dir + name):
+            name += '/'
+        link = urllib.parse.quote(name)
+        text = html.escape(name)
+        html_code = str.format('<a href="%s">%s</a>\r\n' % (link, text))
+        self.content += html_code
+
+    def get_response(self) -> bytes:
+        self.content += ('</pre>\r\n'
+                        '<hr>\r\n'
+                        '</body></html>\r\n')
+        return bytes(self.headers + self.content, 'utf-8')
+
+
+class FileResponse(object):
+    def __init__(self, path, part_range, session_id):
+        self.path = path
+        self.size = os.path.getsize(path)
+        self.start = None
+        self.end = None
+
+        if part_range is not None:
+            self.start, self.end = part_range[0], part_range[1]
+            if self.end < 0:
+                self.end = self.size + self.end
+            self.headers = ('HTTP/1.0 206 Partial Content\r\n'
+                            'Server: GH-Autoindex\r\n')
+            self.headers += 'Content-Type: ' + self.__file_type() + '\r\n'
+            self.headers += str.format('Content-Range: bytes %d-%d/%d\r\n' %
+                                       (self.start, self.end, self.size))
+            self.headers += 'Connection: close\r\n'
+            self.headers += 'Content-Length: ' + str(self.end - self.start + 1) + '\r\n\r\n'
+
+        else:
+            self.headers = ('HTTP/1.0 200 OK\r\n'
+                            'Server: GH-Autoindex\r\n')
+            self.headers += 'Content-Type: ' + self.__file_type() + '\r\n'
+            self.headers += 'Connection: close\r\n'
+            self.headers += 'Content-Length: ' + str(self.size) + '\r\n'
+            self.headers += 'Accept-Ranges: bytes\r\n\r\n'
+
+    def __file_type(self) -> str:
+        f_type = mime_types.get(self.path.split('.')[-1])
+        if not f_type:
+            f_type = 'Application/octet-stream'
+        return f_type
+
+    def get_headers(self) -> bytes:
+        return bytes(self.headers, 'utf-8')
+
+    def get_content(self) -> bytes:
+        file = open(self.path, 'rb')
+        if self.start is not None:
+            file.seek(self.start, 0)
+            ret = file.read(self.end - self.start + 1)
+        else:
+            ret = file.read()
+        file.close()
+        return ret
+
+
+class NonExistResponse(object):
+    def __init__(self):
+        self.response = [b'HTTP/1.0 404 Not Found\r\n',
+                         b'Content-Type:text/html; charset=utf-8\r\n',
+                         b'Server: GH-Autoindex\r\n',
+                         b'Connection: close\r\n',
+                         b'\r\n',
+                         b'<html>\r\n',
+                         b'<head><title>404 Not Found</title></head>\r\n',
+                         b'<body bgcolor="white">\r\n',
+                         b'<center><h1>404 Not Found</h1></center>\r\n',
+                         b'<hr><center>GH-Autoindex/0.0.1</center>\r\n',
+                         b'</body>\r\n',
+                         b'</html>\r\n']
+
+    def get_response(self):
+        return self.response
